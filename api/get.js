77dea42f02f2b86f5340cbdd8406e6a03f89f3df
bema1212 <bouwmanenergie@outlook.com>
2025-01-30 17:16:46 +0100

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+cexport default async function handler(req, res) {
   try {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
